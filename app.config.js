eca677a91490ca10f1e9f50b843421b50c8e34a6
jeoneunjin <pengom0809@gmail.com>
2024-11-12 15:23:46 +0900

    build: scheme 설정

--- app.config.js
+++ app.config.js
@@ -1,6 +1,18 @@
 import 'expo-env';
 
 export default {
+  expo: {
+    name: 'IDBuddy',
+    slug: 'idbuddy',
+    scheme: 'idbuddy', // URI 스킴 설정
+    platforms: ['ios', 'android'],
+    version: '1.0.0',
+    orientation: 'portrait',
+
+    updates: {
+      fallbackToCacheTimeout: 0
+    }
+  },
   extra: {
     videoUri: process.env.VIDEO_URI,
   },
